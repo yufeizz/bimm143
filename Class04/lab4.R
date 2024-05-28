9b57f4de7644f83f6073e42cf69a6174d158b4d9
yufeizz <114945605+yufeizz@users.noreply.github.com>
2024-05-27 21:38:58 -0700

    i added all local hw/r work done previously in week 1-8

---
+++ Class04/lab4.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "BIMM143 Class4: R basics"
+#' author: "Yufei Zhang (PID:A16987415)"
+#' ---
+
+
+#This is just a text file
+x<-1:50
+plot(x,sin(x))
+plot(x,sin(x),typ='l',col="red",lwd=3)
